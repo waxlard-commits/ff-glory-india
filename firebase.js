5ca6fd1ed743d5fe4759867ec37a374f760aa3a9
waxlard-commits <waxlard@gmail.com>
2026-06-27 18:49:58 +0530

    Update Firebase configuration and export syntax

--- firebase.js
+++ firebase.js
@@ -4,18 +4,16 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnXd0HXLM2mcsWm1VUP7BZQDljQwo7L5s",
-  authDomain: "ff-glory-india.firebaseapp.com",
-  projectId: "ff-glory-india",
-  storageBucket: "ff-glory-india.firebasestorage.app",
-  messagingSenderId: "1041307296241",
-  appId: "1:1041307296241:web:777bace6869a6c8aede450"
+  apiKey: "AIzaSyDs3k_Gp2mxCqQ8h0Gtwt6hIlfgDzXZkj0",
+  authDomain: "ffindiaglory.firebaseapp.com",
+  projectId: "ffindiaglory",
+  storageBucket: "ffindiaglory.firebasestorage.app",
+  messagingSenderId: "983153794186",
+  appId: "1:983153794186:web:971e3cd0aad617027b2173"
 };
 
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { auth, db, storage };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
